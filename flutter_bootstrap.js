991857dd7372491e82066598fe24be25d9db4967
rhayonne <rhayonne@users.noreply.github.com>
2026-07-19 11:45:17 +0000

    deploy: v1.7.0+18 (a55d3756a697f91d620b6c1b708a1e84dac736aa) a55d3756a697f91d620b6c1b708a1e84dac736aa

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3651557567" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4216544951" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
